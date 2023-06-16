38a2f0c74844533c5c2a4357a9d269ed9d994829
Hyunsoo Kim <hscornelia999@gmail.com>
2023-06-16 22:18:24 +0900

    FInal result

--- run.sh
+++ run.sh
@@ -1,1 +1,47 @@
 sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=4k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=4k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=4k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=4k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=8k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=8k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=8k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=8k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=16k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=16k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=16k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=16k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=32k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=32k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=32k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=324k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=128k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=128k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=128k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=128k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=256k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=256k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=256k --iodepth=1 --io_size=1G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=256k --iodepth=1 --io_size=1G --name=fio_direct_read_test
