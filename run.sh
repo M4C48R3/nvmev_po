c2005fd7592d74779946413b7947b98de9290f8c
Hyunsoo Kim <hscornelia999@gmail.com>
2023-06-18 16:11:30 +0900

    Fix converged output

--- run.sh
+++ run.sh
@@ -26,11 +26,11 @@ sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=3
 sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=32k --iodepth=1 --io_size=1G --name=fio_direct_read_test
 
 
-sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_write_test
-sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=64k --iodepth=1 --io_size=2G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=read --ioengine=psync --bs=64k --iodepth=1 --io_size=2G --name=fio_direct_read_test
 
-sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_write_test
-sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=64k --iodepth=1 --io_size=1G --name=fio_direct_read_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=write --ioengine=psync --bs=64k --iodepth=1 --io_size=2G --name=fio_direct_write_test
+sudo fio --filename="/dev/nvme0n1" --direct=1 --rw=read --ioengine=psync --bs=64k --iodepth=1 --io_size=2G --name=fio_direct_read_test
 
 
 sudo fio --filename="/dev/nvme1n1" --direct=1 --rw=write --ioengine=psync --bs=128k --iodepth=1 --io_size=2G --name=fio_direct_write_test
